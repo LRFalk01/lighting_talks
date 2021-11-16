0c1c5990dbc86fe44dac342b7806d363d3ae9d7e
lfalk <lfalk@truecar.com>
2021-11-16 08:13:30 -0500

    add positional arity

--- Structs/benchmark.rb
+++ Structs/benchmark.rb
@@ -119,6 +119,31 @@ Benchmark.ips do |x|
   x.compare!
 end
 
+pp 'Positional arity'
+Benchmark.ips do |x|
+  x.report('Positional One Init') do
+    BasicLargeStruct.new(:one)
+  end
+
+  x.report('Positional Two Init') do
+    BasicLargeStruct.new(:one, :two)
+  end
+
+  x.report('Positional Three Init') do
+    BasicLargeStruct.new(:one, :two, :tree)
+  end
+
+  x.report('Positional Four Init') do
+    BasicLargeStruct.new(:one, :two, :tree, :four)
+  end
+
+  x.report('Positional Five Init') do
+    BasicLargeStruct.new(:one, :two, :tree, :four, :five)
+  end
+
+  x.compare!
+end
+
 # "Two property objects"
 # Warming up --------------------------------------
 # BasicStruct Init   681.488k i/100ms
@@ -175,3 +200,26 @@ end
 # Keyword Three Init:  2845869.2 i/s - 1.36x  (± 0.00) slower
 # Keyword Four Init:  2457856.0 i/s - 1.58x  (± 0.00) slower
 # Keyword Five Init:  2226096.1 i/s - 1.74x  (± 0.00) slower
+#
+# "Positional arity"
+# Warming up --------------------------------------
+#  Positional One Init   583.862k i/100ms
+#  Positional Two Init   575.682k i/100ms
+# Positional Three Init
+#                        584.673k i/100ms
+# Positional Four Init   580.748k i/100ms
+# Positional Five Init   595.046k i/100ms
+# Calculating -------------------------------------
+#  Positional One Init      6.073M (± 2.8%) i/s -     30.361M in   5.003465s
+#  Positional Two Init      5.855M (± 3.5%) i/s -     29.360M in   5.021102s
+# Positional Three Init
+#                           5.723M (± 5.0%) i/s -     28.649M in   5.019322s
+# Positional Four Init      5.896M (± 1.8%) i/s -     29.618M in   5.024867s
+# Positional Five Init      5.928M (± 2.7%) i/s -     29.752M in   5.022336s
+#
+# Comparison:
+#  Positional One Init:  6072803.0 i/s
+# Positional Five Init:  5928492.6 i/s - same-ish: difference falls within error
+# Positional Four Init:  5896160.7 i/s - same-ish: difference falls within error
+#  Positional Two Init:  5854787.7 i/s - same-ish: difference falls within error
+# Positional Three Init:  5722905.5 i/s - same-ish: difference falls within error
